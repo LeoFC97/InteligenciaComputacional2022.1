62c6e8330f402430076a74ae456f987bef1263a9
leonardo <leonardo.fc2@outlook.com>
2022-08-04 15:54:03 -0300

    fix

--- coincap/coincap/spiders/cardano.py
+++ coincap/coincap/spiders/cardano.py
@@ -7,9 +7,16 @@ class CardanoSpider(scrapy.Spider):
     allowed_domains = ['https://coinmarketcap.com/currencies/cardano']
     start_urls = ['https://coinmarketcap.com/currencies/cardano/']
 
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"}, callback=self.parse)
+        return super().start_requests()
+
     def parse(self, response):
-        coinPrice=response.xpath('//div[@class="priceValue "]/text()').get()
+        coinPrice=response.xpath('//*[@id="__next"]/div/div[1]/div[2]/div/div[1]/div[2]/div/div[2]/div[1]/div/span/text()').get()
+        print(coinPrice)
         coinTag=response.xpath('//small[@class="nameSymbol"]/text()').get()
+        print(coinTag)
         scraped_info = {
             'tag': coinTag,
             'price': coinPrice,
